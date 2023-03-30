da8baaaf30a1a0d869efbb7fc0eaf90d25e60ea2
VictorySamoletova <victoryer@mail.ru>
2023-03-30 16:44:42 +0300

    Created AsyncTaskExample activity

---
+++ app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
@@ -0,0 +1,14 @@
+package ru.synergy.asyncexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsyncTaskExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task_example);
+    }
+}
--- app/src/main/java/ru/synergy/asyncexample/MainActivity.java
+++ app/src/main/java/ru/synergy/asyncexample/MainActivity.java
@@ -2,7 +2,9 @@ package ru.synergy.asyncexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void buttonClick(View view){
+        switch (view.getId()){
+            case R.id.asynctask:
+                Intent intent = new Intent(this,AsyncTaskExample.class);
+                startActivity(intent);
+        }
+    }
+
 }
